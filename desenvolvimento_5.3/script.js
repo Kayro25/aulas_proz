a488fda739ebe633632380b606f2fdda3b923685
Kayro <me0711405@gmail.com>
2024-02-21 14:12:29 -0300

    desenvolvimento 5.3

---
+++ desenvolvimento_5.3/script.js
@@ -0,0 +1,20 @@
+let h1 = document.createElement("h1")
+h1.innerText = "Produtos"
+h1.id = "titulo"
+
+document.body.appendChild(h1)
+
+let h2 = document.createElement("h2")
+h2.innerText = "Lasanha"
+
+document.body.appendChild(h2)
+
+let desc = document.createElement("p")
+desc.innerText = "Uma lasanha saborosa é um prato de origem italiana, feito com camadas de massa fina, recheio e molho. Existem diversas variações de recheio, como carne, frango, queijo, espinafre, peixe e até macarrão instantâneo. O molho pode ser vermelho, branco, bechamel ou Alfredo. O queijo ralado por cima dá o toque final, deixando a lasanha gratinada e irresistível."
+
+document.body.appendChild(desc)
+
+let preco = document.createElement("p")
+preco.innerText = "R$ 999,99"
+
+document.body.appendChild(preco)
